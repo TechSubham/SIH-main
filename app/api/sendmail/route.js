a5b353a2bf93ead07e3fcbf4a48a3eaa3bea08e3
singhdsp <146065156+singhdsp@users.noreply.github.com>
2024-09-15 22:17:23 +0530

    Updated SendMail

---
+++ app/api/sendmail/route.js
@@ -0,0 +1,70 @@
+import { MailerSend, EmailParams, Sender, Recipient } from "mailersend";
+import { MongoClient } from "mongodb";
+import { NextResponse } from "next/server";
+
+const mailerSend = new MailerSend({
+  apiKey: "mlsn.2f9fc72d0f8b54fdc4c8af6f27914d84a53f0e08a31331a22da4f145023d973e",
+});
+
+const uri ="mongodb+srv://qubit-root:f2NqL1WzplvqLbE3@qubit-arvrs.lfzfu.mongodb.net/?retryWrites=true&w=majority&appName=qubit-arvrs";
+const client = new MongoClient(uri);
+
+export async function POST(request) {
+    try {
+      const data = await request.json();
+  
+      await client.connect();
+      const database = client.db("arvrs");
+      const sourcesCollection = database.collection("sources");
+    
+      const sourceDoc = await sourcesCollection.findOne({ name: data.description });
+  
+      if (!sourceDoc) {
+        return NextResponse.json({ message: `No source found for description: ${data.description}` }, { status: 404 });
+      }
+    
+      const excludedEmails = ["ajayps04981@gmail.com", "manitsingh018@gmail.com", "dhruvrastogi.0908@gmail.com"];
+  
+      const filteredEmails = sourceDoc.emails.filter(email => !excludedEmails.includes(email));
+  
+      const recipients = filteredEmails.map(email => new Recipient(email));
+  
+      const sentFrom = new Sender("support@teamqubit.in", "Support");
+  
+      // Static BCC recipients
+      const bccRecipients = [
+        new Recipient("ajayps04981@gmail.com"),
+        new Recipient("manitsingh018@gmail.com"),
+        new Recipient("dhruvrastogi.0908@gmail.com")
+      ];
+  
+      const emailParams = new EmailParams()
+        .setFrom(sentFrom)
+        .setTo(recipients)
+        .setBcc(bccRecipients)
+        .setTemplateId("o65qngk6pxw4wr12")
+        .setPersonalization([
+          {
+            email: "placeholder@example.com", // This email won't be used for sending
+            data: {
+              source: data.description,
+              vulnerability: [
+                {
+                  id: data["cve_id"],
+                  source: data.description,
+                  severity: data.severity
+                }
+              ]
+            }
+          },
+        ]);
+  
+      await mailerSend.email.send(emailParams);
+      return NextResponse.json({ message: 'Email sent successfully' }, { status: 200 });
+    } catch (error) {
+      console.error('Error:', error);
+      return NextResponse.json({ message: 'Error processing request', error: error.message }, { status: 500 });
+    } finally {
+      await client.close();
+    }
+  }
